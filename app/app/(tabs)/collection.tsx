6cdc7fbc96e360b00f8f9ee79697d3383eeff499
Matheus Moura <matheuscmoura7@gmail.com>
2026-05-31 21:38:30 -0300

    fix: remove duplicate Image import in collection

--- app/app/(tabs)/collection.tsx
+++ app/app/(tabs)/collection.tsx
@@ -1,11 +1,11 @@
 import {
   View, Text, ScrollView, TouchableOpacity,
   Image, StyleSheet, ActivityIndicator, Alert,
+  Modal, TextInput, FlatList,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter, useFocusEffect } from 'expo-router';
 import { useCallback, useState } from 'react';
-import { Modal, TextInput, FlatList, Image } from 'react-native';
 import { Colors } from '../../constants/colors';
 import { useBinders } from '../../hooks/useBinders';
 import { cardsService } from '../../services/cards';
